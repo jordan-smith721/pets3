283461effa05a341e27b04e4013069c6b6878f80
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-01-25 13:38:39 -0500

    added template to order in index.php

--- index.php
+++ index.php
@@ -56,13 +56,13 @@ $f3->route('GET /@animal', function($f3, $params) {
 });
 
 //Define a form1 route
-$f3->route('GET /order', function() {
-    $view = new View();
-    echo $view->render('views/form1.html');
+$f3->route('GET|POST /order', function() {
+    $template = new Template();
+    echo $template->render('views/form1.html');
 });
 
 //Define a form2 route
-$f3->route('POST /order2', function() {
+$f3->route('GET|POST /order2', function() {
     $_SESSION['animal'] = $_POST['animal'];
     $template = new Template();
     echo $template->render('views/form2.html');
