058d749eb5d8ee83d0874f3bd7178d95518022ab
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-01-25 13:48:04 -0500

    created valid color function

--- model/validation-functions.php
+++ model/validation-functions.php
@@ -5,3 +5,12 @@
  * Date: 1/25/2019
  * Time: 10:40 AM
  */
+/* Validate a color
+ *
+ * @param String color
+ * @return boolean
+ */
+function validColor($color) {
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
