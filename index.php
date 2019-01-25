c8e11ddad360954522b113511f3d5593ffdeb403
Jordan Smith <jordan.m.smith721@gmail.com>
2019-01-25 13:35:25 -0500

    added index

--- index.php
+++ index.php
@@ -64,8 +64,8 @@ $f3->route('GET /order', function() {
 //Define a form2 route
 $f3->route('POST /order2', function() {
     $_SESSION['animal'] = $_POST['animal'];
-    $view = new View();
-    echo $view->render('views/form2.html');
+    $template = new Template();
+    echo $template->render('views/form2.html');
 });
 
 //Define a results route
